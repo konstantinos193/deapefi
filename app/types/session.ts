d88793e9c63d5df370c11cac29b7bfca8d8b54cb
konstantinos193 <konstantinosblavakis@gmail.com>
2025-01-05 03:47:42 +0200

    idk any more

--- app/types/session.ts
+++ app/types/session.ts
@@ -5,15 +5,19 @@ export interface Wallet {
   totalNFTs?: number;
   tier?: number;
   totalPoints?: number;
+  walletBalance: number;
+  stakedTokens: number;
+  totalBalance: number;
 }
+
 
 export interface Session {
   id: string;
   sessionId?: string;
-  username: string;
-  discordId: string;
-  isDiscordConnected: boolean;
-  wallets: Wallet[];
+  username?: string;
+  discordId?: string;
+  isDiscordConnected?: boolean;
+  wallets?: Wallet[];
   createdAt: number;
   expiresAt?: number;
 } 
