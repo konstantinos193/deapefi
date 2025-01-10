448793ea54a979ecbe507ec0df4d8b515a07f256
konstantinos193 <konstantinosblavakis@gmail.com>
2025-01-10 03:20:29 +0200

    Update Open Graph description and image in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,12 +21,13 @@ export default function RootLayout({
       <head>
   <meta property="og:title" content="DeApe.fi" />
   <meta property="og:description" content="Borrow APE using your NFT as collateral, and keep your NFT in your wallet. Lend APE to earn more APE, and receive the NFT if the loan isn’t repaid." />
-  <meta property="og:image" content="https://i.imgur.com/XPagcVe.jpeg" />
+  <meta property="og:image" content="https://res.cloudinary.com/dguz3xo20/image/upload/v1736471889/deapefi_ctasig.jpg" />
   <meta property="og:image:width" content="1200" />
   <meta property="og:image:height" content="630" />
   <meta property="og:url" content="https://deape.fi" />
   <meta property="og:type" content="website" />
 </head>
+
 
       <body className="min-h-screen bg-gradient-to-br from-gray-900 via-blue-900 to-gray-900 text-gray-100">
         <WalletProvider>
